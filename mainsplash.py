9ca8f497cccbc525ccc5d29510513ce79c11e9b3
amritaspot <61848493+amritaspot@users.noreply.github.com>
2020-11-10 15:59:09 +0530

    Add files via upload

--- mainsplash.py
+++ mainsplash.py
@@ -245,7 +245,7 @@ class instruction(Screen):
     pass
 
 class resultcardtest(Screen):
-    self.sample_id.text = self.manager.get_screen('sampleid').sample_id
+    self.sample_id.text = root.manager.get_screen('sampleid').sample_id
     self.results.text = str(concentration)
     def saveresults(self):
             cursor.execute("INSERT INTO results (conc_result) VALUES (?)", (concentration))
