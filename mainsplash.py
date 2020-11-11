be57493522de5fb55df726d6adecb304514d4a7f
amritaspot <61848493+amritaspot@users.noreply.github.com>
2020-11-11 14:06:54 +0530

    Add files via upload

--- mainsplash.py
+++ mainsplash.py
@@ -15,7 +15,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import ObjectProperty
+from kivy.properties import NumericProperty, StringProperty, ListProperty
 from kivy.lang import Builder
 from random import random
 from kivy.uix.popup import Popup
@@ -39,42 +39,26 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS results (
          batch_id TEXT
          date TEXT
          time TEXT
-         test_type TEXT
-         assay_type TEXT
          conc_result REAL
-         unit TEXT
          test_image BLOB
      )
      """)
 
+##maybe the calibrations table is not required
 cursor.execute("""CREATE TABLE IF NOT EXISTS calibrations (
-         batch_number TEXT
          test_type TEXT
          assay_type TEXT
+         batch_id TEXT
          unit TEXT
-         pt_1 REAL
-         val_1 REAL
-         pt_2 REAL
-         val_2 REAL
-         pt_3 REAL
-         val_3 REAL
-         pt_4 REAL
-         val_4 REAL
-         pt_5 REAL
-         val_5 REAL
  )""")
 
- #dynamically create calibrations table; dynamic number of rows
-
 conn.commit()
-
 camera = PiCamera()
 
 class mainsplash(Screen):
     pass
 
 class enteruserid(Screen):
-
     def verify_username(self):
         title = "Wrong UserId"
         msg = "Userid is incorrect for Deviceid: VIEAS2003"
@@ -85,7 +69,6 @@ class enteruserid(Screen):
     pass
 
 class enterpassword(Screen):
-
     def verify_password(self):
         title = "Wrong Password"
         msg = "Password is incorrect for Deviceid: VIEAS2003"
@@ -96,57 +79,43 @@ class enterpassword(Screen):
     pass
 
 class choosemode(Screen):
-    # add a screen after choose mode for which test
-    pass
-
-class entersampleid(Screen):
-    global sample_id
-    title = "Existing SampleID"
-    msg = "SampleID already exists. Please enter a different id"
-
-    def verify_sampleid(self):
-        sample_id = self.ids["new_sampleid"].text
-        cursor.execute("""SELECT sample_id
-                   FROM results
-                   WHERE sample_id=?""",
-                (sample_id))
-        check = cursor.fetchone()
-        if result:
-            Popup(self,msg,title)
-        else:
-            self.manager.current='testtype'
+    #make this centered for all buttons and use appropriate padding
     pass
 
 class entertesttype(Screen):
+    #write the dropdown logic for test type
+    pass
+
+class entersampleid(Screen):
+    title = "Existing SampleID"
+    msg = "SampleID already exists. Please enter a different id"
+    def verify_sampleid(self):
+        self.sample_id = self.ids["new_sampleid"].text
+        cursor.execute("""SELECT sample_id
+                   FROM results
+                   WHERE sample_id=?""",
+                (self.sample_id))
+        check = cursor.fetchone()
+        if check == None:
+            self.manager.current='testtype'
+        else:
+            Popup(self,msg,title)
     pass
 
 class enterbatchcode(Screen):
-    global batchid
-    global std_curve
-    global datenow
-    global timenow
-    today = date.today()
-    now = datetime.now()
-    datenow = today.strftime("%B %d, %Y")
-    timenow = now.strftime("%H:%M:%S")
     title = "Batchid Error"
     msg = "Error reading Batchid, please reenter"
     def read_batchid(self):
         try:
-            batchid = self.ids["new_batchid"].text
-            std_curve = decode(batchid)
-            cursor.execute("INSERT INTO results (sample_id, batch_id, date, time, test_type, assay_type, unit) VALUES (?, ?, ?, ?, ?, ?, ?)", (sample_id, batchid, datenow, timenow, testtype, assaytype, unit))
-            conn.commit()
+            self.batchid = self.ids["new_batchid"].text
+            self.stdcurve = decode(self.batchid)
             self.manager.current='instruction'
         except:
             Popup(self,msg,title)
     pass
 
 class instruction(Screen):
-
     def camcapture(self):
-         global concentration
-         global peak_ratio
          try:
              GPIO.setwarnings(False)
              GPIO.setmode(GPIO.BOARD)
@@ -158,22 +127,23 @@ class instruction(Screen):
              camera.capture('/home/pi/view/capturedimage.jpg')
              camera.stop_preview()
              GPIO.output(40,False)
+             input_image = cv2.imread('/home/pi/view/capturedimage.jpg')
+             roi = input_image[30:290, 375:425]
+             cv2.imwrite('/home/pi/view/roi.jpg',roi)
+
          except:
              title = "Camera Error"
              msg = "Error starting camera, please call support"
              Popup(self,msg,title)
-         input_image = cv2.imread('/home/pi/view/capturedimage.jpg')
-         roi = input_image[30:290, 375:425]
-         cv2.imwrite('/home/pi/view/roi.jpg',roi)
+
          try:
              results_array = mov_avgscan(roi)
-             peak_ratio = calc_ratio(results_array)
-             concentration = calconc(peak_ratio, std_curve)
+             self.peakratio = calc_ratio(results_array)
+             self.concentration = calconc(self.peakratio, self.stdcurve)
          except:
              title = "Error reading test"
              msg = "Please ensure test has run properly"
              Popup(self,msg,title)
-         return concentration
 
     def mov_avgscan(final_image):
          input=final_image
@@ -214,53 +184,33 @@ class instruction(Screen):
          points_array.sort()
          n = len(points_array)-1
          print(points_array[n], points_array[n-1])
-         peakratio = points_array[n-1]/points_array[n]
-         return peakratio
+         peak_ratio = points_array[n-1]/points_array[n]
+         return peak_ratio
 
     def calconc(peakratio, stdcurve):
          slope = stdcurve[0]
+         print('slope', slope)
          intercept = stdcurve[1]
+         print('intercept', intercept)
          conc = slope*peakratio+intercept
+         print('concentration', conc)
          if (conc<0):
              conc = 0
          return conc
-
-    def decode(batchid):
-         global unit
-         mainstr = int(batchid, 16)
-         number = str(number)
-         conc1 = int(number[0:1])
-         au1 = float(number[2:4])/100
-         conc2 = int(number[5:6])
-         au2 = float(number[7:9])/100
-         conc3 = int(number[10:11])
-         au3 = float(number[12:14])/100
-         unit = "10^-"+number[15]+"mg/ml"
-         x=np.array([conc1, conc2, conc3])
-         y=np.array([au1, au2, au3])
-         m,b = np.polyfit(x, y, 1)
-         decoded = [m,b]
-         return decoded
-
     pass
 
 class resultcardtest(Screen):
-
-    #self.ids["sample_id"].text = entersampleid.sample_id
-    #self.ids["results"].text = instruction.concentration
-    #self.ids["date"].text = enterbatchcode.datenow
-    #self.ids["time"].text = enterbatchcode.timenow
+    today = date.today()
+    now = datetime.now()
+    datenow = today.strftime("%B %d, %Y")
+    timenow = now.strftime("%H:%M:%S")
+    input_image = cv2.imread('/home/pi/view/roi.jpg')
     def saveresults(self):
-            cursor.execute("INSERT INTO results (conc_result) VALUES (?)", (concentration))
+            cursor.execute("INSERT INTO results (sample_id, batch_id, date, time, conc_result, test_image) VALUES (?)", (self.sample_id, self.batchid, date, time, self.concentration, input_image))
             conn.commit()
             self.manager.current='modes'
-
+            print('concentrationdb', self.concentration)
     def discardresults(self):
-            cursor.execute("""DELETE
-                       FROM results
-                       WHERE sample_id=?""",
-                    (sample_id))
-            conn.commit()
             self.manager.current='modes'
     pass
 
@@ -268,9 +218,6 @@ class instructionc(Screen):
     pass
 
 class enterconccard(Screen):
-    global cal_conc
-    global cal_au
-    global batchid
     def pkratio():
         input_image = startcam()
         roi = input_image[30:290, 375:425]
@@ -281,13 +228,17 @@ class enterconccard(Screen):
         return rndpr
 
     def read_testassay(self):
-        cal_conc = np.append(cal_conc, self.ids["new_concid"].text)
+        conc = self.ids["new_concid"].text
         peak_ratio = pkratio()
-        cal_au = np.append(cal_au, peak_ratio)
+        self.calibration_array.append([conc, peak_ratio])
         self.ids["new_concid"].text = ""
 
     def gencode():
-        [m,b] = np.polyfit(cal_conc, cal_au, 1)
+        l=self.calibration_array
+        for i in l:
+            conc_array = conc_array.append(i[0])
+            pr_array = pr_array.append(i[1])
+        [m,b] = np.polyfit(conc_array, pr_array, 1)
         batchid = str(m)+"/"+str(b)
         print(batchid)
         self.manager.current = 'newcode'
@@ -303,6 +254,11 @@ class resultview(Screen):
         self.rows = cursor.fetchall()
         print(self.rows)
     pass
+
+def decode(batchid):
+     decoded = batchid.split["/"]
+     print(decoded, 'decoded')
+     return decoded
 
 def PopUp(self, msg, title):
     box = BoxLayout(orientation = 'vertical', padding = (10))
@@ -344,6 +300,12 @@ sm.add_widget(generatebatchcode(name='newcode'))
 sm.add_widget(resultview(name='history'))
 
 class MainApp(App):
+    sample_id = StringProperty('')
+    batchid = StringProperty('')
+    concentration = NumericProperty(1.0)
+    peakratio = NumericProperty(1.0)
+    stdcurve = ListProperty([0,0])
+    calibration_array = ListProperty([0,0])
     def build(self):
         Window.size = (800, 500)
         return sm
