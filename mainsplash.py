29cb1d36484b32bae6cd9fd7d81a5b388ad80e3d
amritaspot <61848493+amritaspot@users.noreply.github.com>
2020-11-12 21:50:19 +0530

    Add files via upload

--- mainsplash.py
+++ mainsplash.py
@@ -261,13 +261,16 @@ class resultcardtest(Screen):
         print(self.batch_value.batch_id)
         self.ids["results"].text = str(self.result_value.concentration)
     def saveresults(self):
-        input_image = cv2.imread('/home/pi/view/roi.jpg')
-        conn = sqlite3.connect('tests.db')
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO results (sample_id, batch_id, date, time, conc_result, test_image) VALUES (?,?,?,?,?,?)", (self.sample_value.sample_id, self.batch_value.batch_id, self.datenow, self.timenow, self.result_value.concentration, input_image))
-        conn.commit()
-        conn.close()
-        self.manager.current='modes'
+        try:
+            input_image = cv2.imread('/home/pi/view/roi.jpg')
+            conn = sqlite3.connect('tests.db')
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO results (sample_id, batch_id, date, time, conc_result, test_image) VALUES (?,?,?,?,?,?)", (self.sample_value.sample_id, self.batch_value.batch_id, self.datenow, self.timenow, self.result_value.concentration, input_image))
+            conn.commit()
+            conn.close()
+            self.manager.current='modes'
+        except:
+            self.manager.current='modes'
     def discardresults(self):
         self.manager.current='modes'
     pass
